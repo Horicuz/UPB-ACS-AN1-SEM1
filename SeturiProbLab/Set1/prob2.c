cd4d74c7f2b747771a6e702e4b1df18eb42014eb
Horicuz <132096814+Horicuz@users.noreply.github.com>
2023-10-14 18:05:36 +0300

    Create prob2.c

---
+++ SeturiProbLab/Set1/prob2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main 
+{
+    float a,b,c;
+    float d = 5;
+    scanf("%f%f",&a,&b);
+    printf("%.3f %.3f\n", a, b);
+    printf("%.5f %.5f\n", a, b);
+    printf("%.2f %.2f\n", a, b);
+    printf("%e\n", a);
+    printf("%d\n", a+b);
+    scanf("%d", &c);
+    printf("%d\n", c);
+    printf("%.3f\n", d);
+}
