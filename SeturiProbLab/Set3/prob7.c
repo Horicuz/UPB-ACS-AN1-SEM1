8621f20c7d51e4fe3f66183f7c82007c33cdaef5
Horicuz <132096814+Horicuz@users.noreply.github.com>
2023-10-24 01:17:42 +0300

    Update prob7.c
    better stronger

--- SeturiProbLab/Set3/prob7.c
+++ SeturiProbLab/Set3/prob7.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 int main()
@@ -8,24 +9,83 @@ int main()
     for(int i = 0; i<n; i++)
         for(int j = 0; j<n; j++)
             scanf("%d", &a[i][j]);
-
+    printf("\n\n");
     for(int i = 0; i<n; i++)
     {
         for(int j = 0; j<n; j++)
             printf("%d ", a[i][j]);
         printf("\n");
     }
-
-     for(int i = 0; i<n; i++)
+    printf("\n\n");
+    for(int i = 0; i<n; i++)
     {
-       printf("%d ", a[i][i]);
+        printf("%d ", a[i][i]);
     }
     printf("\n\n");
     for(int i = 0; i<n; i++)
     {
-       printf("%d ", a[i][n-1-i]);
+        printf("%d ", a[i][n-1-i]);
     }
-     printf("\n\n");
+    printf("\n\n");
+
+    // triunghi superior
+     for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            if(j>i)
+                printf("%d ", a[i][j]);
+            else printf("  ");
+        }
+        printf("\n");
+    }
+    //triunghi inferior
+
+     for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            if(j<i)
+                printf("%d ", a[i][j]);
+            else printf("  ");
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            if(i == 0 || j == 0 || i == n-1 || j == n-1)
+                printf("%d ", a[i][j]);
+            else printf("  ");
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+
+    int colstart = 0;
+    int colend = n-1;
+    int rowstart = 0;
+    int rowend = n-1;
+    while(colstart != colend && rowstart!= rowend)
+    {
+        for(int i = colstart;i <= colend;i++)
+            printf("%d ", a[rowstart][i]);
+        rowstart = rowstart + 1;
+        for(int i = rowstart;i<= rowend;i++)
+            printf("%d ", a[i][colend]);
+        colend -- ;
+        for(int i = colend;i>= colstart; i --)
+            printf("%d ", a[rowend][i]);
+        rowend --;
+        for(int i = rowend;i>= rowstart; i --)
+            printf("%d ", a[i][colstart]);
+        colstart++;
+
+    }
 
 
 }
